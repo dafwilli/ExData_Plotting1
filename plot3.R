68461184aaae76b556053f1c6ed857d1e1968b43
dafwilli <dafwilli@yahoo.com>
2015-02-04 12:39:30 +0100

    Final commit

--- plot3.R
+++ plot3.R
@@ -1,15 +1,14 @@
-##  This analysis reads in data from XXX and creates a series of graphs
+##  This analysis reads in data from the UC Irvine Machine Learning Repositoy and creates a series of graphs
 
 #  Read in data - file should be in the working directory
 data <- read.table("./household_power_consumption.txt",header=TRUE,sep=";",na.strings = "?",colClasses = c(rep("character",2),rep("numeric",7)))
 
-# Convert the Date variable to a POSIXlt format
-# data$Date <- strptime(data$Date,"%d/%m/%y")
-data <- mutate(data,Date = dmy(Date))
-
 # Load the dplyr and lubridate library
 library("dplyr")
 library("lubridate")
+
+# Convert the Date variable to a POSIXlt format
+data <- mutate(data,Date = dmy(Date))
 
 # Reduce the data set to only data from the dates 2007-02-01 and 2007-02-02
 data <- filter (data, Date == ymd("2007-02-01") | Date == ymd("2007-02-02"))
--- plot4.R
+++ plot4.R
@@ -1,15 +1,14 @@
-##  This analysis reads in data from XXX and creates a series of graphs
+##  This analysis reads in data from the UC Irvine Machine Learning Repositoy and creates a series of graphs
 
 #  Read in data - file should be in the working directory
 data <- read.table("./household_power_consumption.txt",header=TRUE,sep=";",na.strings = "?",colClasses = c(rep("character",2),rep("numeric",7)))
 
-# Convert the Date variable to a POSIXlt format
-# data$Date <- strptime(data$Date,"%d/%m/%y")
-data <- mutate(data,Date = dmy(Date))
-
 # Load the dplyr and lubridate library
 library("dplyr")
 library("lubridate")
+
+# Convert the Date variable to a POSIXlt format
+data <- mutate(data,Date = dmy(Date))
 
 # Reduce the data set to only data from the dates 2007-02-01 and 2007-02-02
 data <- filter (data, Date == ymd("2007-02-01") | Date == ymd("2007-02-02"))
@@ -18,10 +17,11 @@ data <- filter (data, Date == ymd("2007-02-01") | Date == ymd("2007-02-02"))
 x <- data$Date + hms(data$Time)
 
 # set up for 4 graphs
+png(filename = "plot4.png")
 par(mfrow = c(2,2))
 
-# Create Plot #1 - histogram of Global Active Power
-hist(data$Global_active_power, col = "red", main = "Global Active Power", xlab = "Global Active Power (killowatts)", ylab = "Frequency" )
+# Create Plot #1
+plot(x,data$Global_active_power, type = "l", xlab = "", ylab = "Global Active Power (kilowatts)")
 
 # Create Plot #2
 plot(x,data$Voltage, type = "l", xlab = "dateTime", ylab = "Voltage")
@@ -33,5 +33,6 @@ lines(x,data$Sub_metering_3, col = "blue", type = "l")
 legend("topright", legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), lty = 1, col = c("black", "red", "blue"))
 
 # Create Plot #4
-png(filename = "plot4.png")
+
 plot(x,data$Global_reactive_power, type = "l", xlab = "dateTime", ylab = "Global_reactive_power")
+dev.off()
